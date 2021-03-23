a6ca3756d8863d07aff7459fb29651cf6dd2c4a6
Admin <nazarkharechko90@gmail.com>
2021-03-23 16:46:52 +0200

    manager

--- app/manager.py
+++ app/manager.py
@@ -8,8 +8,8 @@ manager = Manager(app)
 admin = {'id': 1, 'email': 'admin@gmail.com',
          'password': 'admin2020', 'admin': 1}
 
-recruiter = {'id': 1, 'email': 'admin@gmail.com',
-             'password': 'admin2020', 'name': 'Bob', 'profession': 'Js', 'status': 1}
+recruiter = {'id': 1, 'email': 'recruiter@gmail.com',
+             'password': 'recruiter2020', 'name': 'Bob', 'profession': 'Js', 'status': 1}
 
 positions = {'id': 1, 'positions': 'Python',
              'description': 'Python — інтерпретована оcscscs', 'start_date': '2021-03-02 12:06:00',
